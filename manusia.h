1aaa020ef51f9869109904462b871d0537459be9
deoramadanu <deo121005@gmail.com>
2025-05-26 23:18:07 +0700

    menambahkan kelas manusia

--- manusia.h
+++ manusia.h
@@ -4,18 +4,3 @@
 using namespace std;
 
 class manusia {
-public:
-    string name;
-    jantung varJantung;
-
-    manusia(string pName)
-        : name(pName) {
-        cout << name << " hidup\n";
-    }
-
-    ~manusia() {
-        cout << name << " mati\n";
-    }
-};
-
-#endif
