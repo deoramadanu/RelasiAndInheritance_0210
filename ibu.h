04d078013e2282d14ad066416aa8bab34e09d2fa
deoramadanu <deo121005@gmail.com>
2025-05-26 23:26:43 +0700

    add library

--- ibu.h
+++ ibu.h
@@ -4,29 +4,4 @@
 #include <vector>
 #include "anak.h"
 
-class ibu {
-public:
-    string nama;
-    vector<anak*> daftarAnak;
 
-    ibu(string pNama) : nama(pNama) {
-        cout << "Ibu \"" << nama << "\" ada\n";
-    }
-
-    ~ibu() {
-        cout << "Ibu \"" << nama << "\" tidak ada\n";
-    }
-
-    void tambahAnak(anak* pAnak) {
-        daftarAnak.push_back(pAnak);
-    }
-
-    void cetakAnak() {
-        cout << "Daftar anak dari ibu \"" << nama << "\":\n";
-        for (auto& a : daftarAnak) {
-            cout << "- " << a->nama << "\n";
-        }
-    }
-};
-
-#endif
